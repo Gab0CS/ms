7f9bf26af7d954d1a36d847ae4992a5bd6e9d322
Gab0CS <gabocanseco@gmail.com>
2026-04-05 22:26:27 -0600

    report ms first integration

---
+++ report-ms/src/main/java/com/marte/report_ms/models/WebSite.java
@@ -0,0 +1,15 @@
+package com.marte.report_ms.models;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class WebSite implements Serializable {
+
+    private Long id;
+    private String name;
+    private Category category;
+    private String description;
+
+}
